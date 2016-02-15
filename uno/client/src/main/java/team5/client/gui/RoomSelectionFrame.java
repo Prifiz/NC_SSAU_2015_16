b5faff567305d7ed9990dfe0fdfdf7e1d537fee4
Morgan163 <styart.mc.ne@gmail.com>
2016-02-15 23:04:45 +0400

    Добавил сообщение об ожидании других игроков. И сообщение о вашем ходе в гейм фрейме

--- uno/client/src/main/java/team5/client/gui/RoomSelectionFrame.java
+++ uno/client/src/main/java/team5/client/gui/RoomSelectionFrame.java
@@ -173,7 +173,7 @@ public class RoomSelectionFrame extends JFrame {
             }
             switch (command) {
                 case "Wait": {
-                    JOptionPane.showConfirmDialog(null, "Wait for other players",
+                    JOptionPane.showConfirmDialog(null, "Wait for other players more than 30 seconds",
                             "Waiting...", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
                     break;
                 }
