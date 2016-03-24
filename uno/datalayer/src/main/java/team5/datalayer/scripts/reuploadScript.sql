3f340783e4169ce926f3e7784cec219a7b315365
Оля Моссоулина <chanta595@gmail.com>
2016-03-24 21:18:46 +0400

    Скрипты сценариев 1. Перезаливка 2. Стартовое заполнение БД

---
+++ uno/datalayer/src/main/java/team5/datalayer/scripts/reuploadScript.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  chanta
+ * Created: 24.03.2016
+ */
+
+\i DROP_DATA_BASE_CASCADE.sql
+\i CREATE_DATA_BASE.sql
+\i INSERT_DATA_BASE.sql
